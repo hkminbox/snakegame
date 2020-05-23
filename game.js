1513bcf78da5b47271eab09d7faf841bb6b8e655
hkminbox <62156241+hkminbox@users.noreply.github.com>
2020-05-23 22:53:25 +0530

    Libraries added, keypress started working

--- game.js
+++ game.js
@@ -2,13 +2,24 @@ function Game()
 {
 	this.x = 0;
 	this.y = 0;
-	this.speedxdir = 1;
-	this.speedydir = 0;
+	this.xspeed = 1;
+	this.yspeed = 0;
+	this.directionFn = function(x, y)
+	{
+		this.xspeed = x;
+		this.yspeed = y;
 
-}
+	}
 
-function show()
-{
-	fill(255);
-	
+	this.update = function()
+	{
+		this.x= this.x + this.xspeed;
+		this.y= this.y + this.yspeed;
+	}
+
+	this.show = function()
+	{
+		fill(255);
+		rect(this.x,this.y,10,10);
+	}
 }
--- window.js
+++ window.js
@@ -1,9 +1,9 @@
-var s;
+let s;
 
 function setup()
 {
 	createCanvas(640,640);
-	s= new Game();
+	s = new Game();
 }
 
 function draw()
@@ -12,23 +12,50 @@ function draw()
 	s.update();
 	s.show();
 }
+function keyPressed()
+{
+
+  switch(keyCode)
+  {
+  	case UP_ARROW:
+  		s.directionFn(0, -1);
+  		break;
+    case DOWN_ARROW:
+    	s.directionFn(0, 1);
+    	break;
+  	case RIGHT_ARROW:
+    	s.directionFn(1, 0);
+    	break
+  	case LEFT_ARROW:
+	    s.directionFn(-1, 0);
+	    break;
+  }	
+}
+
+
 
 function Game()
 {
-	this.x = 0;
-	this.y = 0;
-	this.speedxdir = 1;
-	this.speedydir = 0;
+  this.x = 0;
+  this.y = 0;
+  this.xspeed = 1;
+  this.yspeed = 0;
+  this.directionFn = function(x, y)
+  {
+    this.xspeed = x;
+    this.yspeed = y;
 
-	this.update = function()
-	{
-		this.x= this.x + this.speedxdir;
-		this.y= this.y + this.speedydir;
-	}
+  }
 
-	this.show = function()
-	{
-		fill(255);
-		rect(this.x,this.y,10,10);
-	}
+  this.update = function()
+  {
+    this.x= this.x + this.xspeed;
+    this.y= this.y + this.yspeed;
+  }
+
+  this.show = function()
+  {
+    fill(255);
+    rect(this.x,this.y,10,10);
+  }
 }
