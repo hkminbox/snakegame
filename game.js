1720e7f2dfba437e9d7f9b8560b15081aeb9b56c
hkminbox <62156241+hkminbox@users.noreply.github.com>
2020-05-24 10:25:18 +0530

    speed var changes

--- game.js
+++ game.js
@@ -2,19 +2,19 @@ function Game()
 {
 	this.x = 0;
 	this.y = 0;
-	this.xspeed = 1;
-	this.yspeed = 0;
+	this.speedxdir = 1;
+	this.speedydir = 0;
 	this.directionFn = function(x, y)
 	{
-		this.xspeed = x;
-		this.yspeed = y;
+		this.speedxdir = x;
+		this.speedydir = y;
 
 	}
 
 	this.update = function()
 	{
-		this.x= this.x + this.xspeed;
-		this.y= this.y + this.yspeed;
+		this.x= this.x + this.speedxdir;
+		this.y= this.y + this.speedydir;
 	}
 
 	this.show = function()
--- window.js
+++ window.js
@@ -31,31 +31,3 @@ function keyPressed()
 	    break;
   }	
 }
-
-
-
-function Game()
-{
-  this.x = 0;
-  this.y = 0;
-  this.xspeed = 1;
-  this.yspeed = 0;
-  this.directionFn = function(x, y)
-  {
-    this.xspeed = x;
-    this.yspeed = y;
-
-  }
-
-  this.update = function()
-  {
-    this.x= this.x + this.xspeed;
-    this.y= this.y + this.yspeed;
-  }
-
-  this.show = function()
-  {
-    fill(255);
-    rect(this.x,this.y,10,10);
-  }
-}
