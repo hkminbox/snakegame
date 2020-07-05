95fd30fc6da50b3e132340a8e9bcee355b2af48a
unknown <gohan001ac@gmail.com>
2020-07-05 11:59:27 +0530

    Frame rate set

--- window.js
+++ window.js
@@ -1,9 +1,11 @@
 let s;
+let scal = 20;
 
 function setup()
 {
 	createCanvas(640,640);
 	s = new Game();
+  frameRate(10);
 }
 
 function draw()
@@ -30,4 +32,33 @@ function keyPressed()
 	    s.directionFn(-1, 0);
 	    break;
   }	
+}
+
+function Game()
+{
+  this.x = 0;
+  this.y = 0;
+  this.speedxdir = 1;
+  this.speedydir = 0;
+  this.directionFn = function(x, y)
+  {
+    this.speedxdir = x;
+    this.speedydir = y;
+
+  }
+
+  this.update = function()
+  {
+    this.x = this.x + this.speedxdir* scal;
+    this.y = this.y + this.speedydir* scal;
+    this.x = constrain(this.x, 0 , width - scal);
+    this.y = constrain(this.y, 0 , height - scal);
+
+  }
+
+  this.show = function()
+  {
+    fill(255);
+    rect(this.x,this.y,10,10);
+  }
 }
